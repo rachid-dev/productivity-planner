166de4fad2f6a084a0e90e93dc5ef64d33ddaa2f
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-01-25 06:25:09 +0000

    feat(Home): Add page component Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './visitor/home/home.page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   title: 'Productivity Planner',
+        path: '',
+        component: HomePageComponent
+    }
+];
