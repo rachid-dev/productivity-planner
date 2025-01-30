f8d89cafb5293756d4343f5233e98bd9d94b02d0
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-01-30 08:14:45 +0000

    Add home banner to home page component

--- src/app/visitor/home/home.page.component.ts
+++ src/app/visitor/home/home.page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeBannerDumbComponent } from "./home-banner/home-banner.dumb.component";
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [HomeBannerDumbComponent],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss'
 })
 export class HomePageComponent {
-
+  onBannerClicked(){
+    console.log("Banner clicked");
+  }
 }
