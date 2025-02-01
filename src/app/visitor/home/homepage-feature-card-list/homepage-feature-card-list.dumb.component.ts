78dad416a1d1cb40fb8060858b6237c3c8ece5e6
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-02-01 21:51:35 +0000

    feature(home-feature-card-list): create new dumb componenent

---
+++ src/app/visitor/home/homepage-feature-card-list/homepage-feature-card-list.dumb.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { FeatureCardList } from './homepage-feature-card-list.interface';
+
+@Component({
+  selector: 'app-homepage-feature-card-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage-feature-card-list.dumb.component.html',
+  styleUrl: './homepage-feature-card-list.dumb.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomepageFeatureCardListDumbComponent {
+  readonly featureCardList = input.required<FeatureCardList>();
+}
