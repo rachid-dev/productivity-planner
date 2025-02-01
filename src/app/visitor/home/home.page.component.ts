1ed0f29e323d495e7cce3cab2088d92498ffdab6
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-02-01 21:56:31 +0000

    Add HomepageFeatureCardList to home-page

--- src/app/visitor/home/home.page.component.ts
+++ src/app/visitor/home/home.page.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HomeBannerDumbComponent } from "./home-banner/home-banner.dumb.component";
+import { HomepageFeatureCardListDumbComponent } from "./homepage-feature-card-list/homepage-feature-card-list.dumb.component";
 
 @Component({
   standalone: true,
-  imports: [HomeBannerDumbComponent],
+  imports: [HomeBannerDumbComponent, HomepageFeatureCardListDumbComponent],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss'
 })
 export class HomePageComponent {
+
+  featureCardList = signal([
+    {
+      name: 'Planifier sa semaine',
+      icon: 'calendar-heart-fill',
+      description: 'Visibilité sur les 7 prochains jours',
+    },
+    {
+      name: 'Atteindre ses objectifs',
+      icon: 'trophy-fill',
+      description: 'Priorisation des tâches',
+    },
+    {
+      name: 'Analyser sa productivité',
+      icon: 'bar-chart-line-fill',
+      description: 'Visualiser le travail accomplis',
+    }
+  ]);
+
   onBannerClicked(){
     console.log("Banner clicked");
   }
