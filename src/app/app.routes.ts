dacc7d734377df65ef44feaa8c06cdce368d19de
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-02-04 17:42:18 +0000

    wip(signup): add route and UI link for redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './visitor/home/home.page.component';
+import { SignupPageComponent } from './visitor/signup/signup.page.component';
 
 export const routes: Routes = [
     {   title: 'Productivity Planner',
         path: '',
         component: HomePageComponent
+    },
+    {   title: 'Signup',
+        path: 'signup',
+        component: SignupPageComponent
     }
 ];
