4ef8c34b51f472a71915cfaca5fd77c0f8098eda
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-02-24 11:51:54 +0000

    wip(signup): add signup component

--- src/app/visitor/signup/signup.page.component.ts
+++ src/app/visitor/signup/signup.page.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../core/authentication.service';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.page.component.html',
   styleUrl: './signup.page.component.scss'
 })
 export class SignupPageComponent {
+  readonly authenticationService = inject(AuthenticationService);
+  readonly name = signal("");
+  readonly email = signal("");
+  readonly password = signal("");
+  readonly confirmPassword = signal("");
+  readonly isPasswordMatchValid = computed(()=>(this.password() === this.confirmPassword()))
+  readonly router = inject(Router);
+  
 
+  onSubmit(){
+    this.authenticationService.register(this.email(), this.password()).subscribe(response => (
+      console.log(response)
+    ))
+    // this.router.navigate(['dashboard']);
+  }
 }
