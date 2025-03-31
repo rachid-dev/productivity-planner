ae62db8de18dc764f6ababbd0fd159d6bb8ee35f
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-03-31 14:22:19 +0000

    Update user.store.ts

--- src/app/core/store/user.store.ts
+++ src/app/core/store/user.store.ts
@@ -1,28 +1,56 @@
 import { computed, inject } from '@angular/core';
-import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { AuthenticationService } from '../port/authentication.service';
 import { User, Visitor } from '../entity/user.interface';
-import { RegisterUserUseCaseService } from '../use-case/register-user.use-case.service';
+import { UserService } from '../port/user.service';
 
-
-interface UserStoreState {
-  user : User | undefined
-};
-
-
+interface UserState {
+  user: User | undefined;
+}
 
 export const UserStore = signalStore(
-  {providedIn : 'root'},
-  withState<UserStoreState>({user : undefined}),
+  { providedIn: 'root' },
+  withState<UserState>({
+    user: undefined,
+  }),
   withComputed((store) => {
-    const isGoogleUser = computed(() => store.user()?.email.endsWith('@google.com'));
-    return {isGoogleUser};
+    const isGoogleUser = computed(
+      () => !!store.user()?.email.endsWith('@google.com')
+    );
+
+    return { isGoogleUser };
   }),
   withMethods(
-    (store, registerUserUseCase = inject(RegisterUserUseCaseService)) => {
-      const register = (visitor : Visitor):void => {
-        registerUserUseCase.execute(visitor).then((user) => {patchState(store, {user})})
-      }
-      return {register}
-    }
+    (
+      store,
+      authenticationService = inject(AuthenticationService),
+      userService = inject(UserService)
+    ) => ({
+      register(visitor: Visitor): void {
+        authenticationService
+          .register(visitor.email, visitor.password)
+          .subscribe((response) => {
+            const user: User = {
+              id: response.userId,
+              name: visitor.name,
+              email: visitor.email,
+            };
+
+            userService.create(user, response.jwtToken).subscribe({
+              next: () => {
+                patchState(store, { user });
+              },
+            });
+          });
+        // 👇 Updating state using the `patchState` function.
+        // patchState(store, (state) => ({ filter: { ...state.filter, query } }));
+      },
+    })
   )
 );
