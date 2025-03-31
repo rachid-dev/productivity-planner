57e53057ba5b9ff98918c50a8179eaf256032356
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-03-31 16:05:15 +0000

    Update authentication-firebase.service.ts

--- src/app/core/adapter/authentication-firebase.service.ts
+++ src/app/core/adapter/authentication-firebase.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
-import { environment } from '@env/environment.dev';
+import { environment } from '@env/environment';
 import { AuthenticationService, LoginResponse, RegisterResponse } from '../port/authentication.service';
 
 /**
