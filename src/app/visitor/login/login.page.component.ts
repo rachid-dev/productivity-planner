4dbe987bd412a3f32696ea656eabed3122c347ad
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-04-17 20:25:33 +0000

    lint(login): apply lint suggestion

--- src/app/visitor/login/login.page.component.ts
+++ src/app/visitor/login/login.page.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LoginUserUseCase } from './domain/login-user.use-case';
 import { InvalidCredentialError } from './domain/invalid-credential.error';
