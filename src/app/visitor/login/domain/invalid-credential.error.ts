f9d967cf2a24ad7f312cfd54418cca4903a5eee2
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-04-17 20:12:38 +0000

    add name property to class

--- src/app/visitor/login/domain/invalid-credential.error.ts
+++ src/app/visitor/login/domain/invalid-credential.error.ts
@@ -1,5 +1,6 @@
 export class InvalidCredentialError extends Error{
     constructor(){
         super("Invalid credentials. Please check your email and password.");
+        this.name = 'InvalidCredentialError';
     }
 }
