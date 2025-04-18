9f62c4c34df107e37a8c31759f4947a9a3a81b9e
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-04-18 11:18:30 +0000

    feat(webapp): add auto-connect initializer strategy

--- src/app/membership/dashboard/dashboard.page.component.ts
+++ src/app/membership/dashboard/dashboard.page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserStore } from '@app/core/store/user.store';
 
 @Component({
   imports: [],
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class DashboardPageComponent {
 
+  readonly store = inject(UserStore);
 }
