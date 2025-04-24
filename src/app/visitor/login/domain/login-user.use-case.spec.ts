7ca75019fd3a7094618c41dbb9668d06a1dd1cb0
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-04-24 14:21:54 +0000

    test(login-use-case) : implement units test

--- src/app/visitor/login/domain/login-user.use-case.spec.ts
+++ src/app/visitor/login/domain/login-user.use-case.spec.ts
@@ -5,36 +5,91 @@ import { AuthenticationService } from '@app/core/port/authentication.service';
 import { UserService } from '@app/core/port/user.service';
 import { UserStore } from '@app/core/store/user.store';
 import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { InvalidCredentialError } from './invalid-credential.error';
 
 describe('LoginUserUseCaseService', () => {
-  let service: LoginUserUseCase;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers : [
-        LoginUserUseCase,
-        { provide: AuthenticationService, useValue: { register: jest.fn()}},
-        { provide: UserService, useValue: { create: jest.fn( )}},
-        { provide: UserStore, useValue: { register: jest.fn() }},
-        { provide: Router, useValue: { navigate: jest.fn() }},
-      ]
-    });
-    service = TestBed.inject(LoginUserUseCase);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  let loginUserUseCase: LoginUserUseCase;
+  const mockUserId = '123';
+  const mockJwtToken = 'jwt-token';
+  const mockJwtRefreshToken = 'refresh-token';
+  const mockExpiresIn = '3600';
+  const mockLoginPayload = {
+    userId: mockUserId,
+    jwtToken: mockJwtToken,
+    jwtRefreshToken: mockJwtRefreshToken,
+    expiresIn: mockExpiresIn,
+  }
+  const mockUser = { id: mockUserId, name: 'John Doe' };
 
   describe('when user provides valid credentials', () => {
-    it.todo('should authenticate the user via AuthenticationService');
-    it.todo('should store jwt tokens in localStorage');
-    it.todo('should fetch the user info via UserService');
-    it.todo('should load the user into the store');
-    it.todo('should navigate to dashboard');
+    
+    const email = "john.doe@acme.com";
+    const password = "Azerty1@";
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers : [
+          LoginUserUseCase,
+          { provide: AuthenticationService, useValue: { login: jest.fn().mockReturnValue(of(mockLoginPayload))}},
+          { provide: UserService, useValue: { fetch: jest.fn().mockReturnValue(of(mockUser))}},
+          { provide: UserStore, useValue: { load: jest.fn() }},
+          { provide: Router, useValue: { navigate: jest.fn() }},
+        ]
+      });
+      loginUserUseCase = TestBed.inject(LoginUserUseCase);
+    });
+
+    it('should authenticate the user via AuthenticationService', async () => {
+      const authenticationService = TestBed.inject(AuthenticationService);
+      await loginUserUseCase.execute(email, password);
+      expect(authenticationService.login).toHaveBeenCalledWith(email, password);   
+    });
+
+    it('should store jwt tokens in localStorage', async () => {
+      await loginUserUseCase.execute(email, password);
+      expect(localStorage.getItem('jwtToken')).toBe(mockJwtToken);
+      expect(localStorage.getItem('jwtRefreshToken')).toBe(mockJwtRefreshToken);
+      expect(localStorage.getItem('expiresIn')).toBe(mockExpiresIn);
+    });
+
+    it('should fetch the user info via UserService', async () => {
+      const userService = TestBed.inject(UserService);
+      await loginUserUseCase.execute(email, password);
+      expect(userService.fetch).toHaveBeenCalledWith(mockUserId, mockJwtToken);
+    });
+
+    it('should load the user into the store', async () => {
+      const userStore = TestBed.inject(UserStore);
+      await loginUserUseCase.execute(email, password);
+      expect(userStore.load).toHaveBeenCalledWith(mockUser);
+    });
+
+    it('should navigate to dashboard', async () => {
+      const router = TestBed.inject(Router);
+      await loginUserUseCase.execute(email, password);
+      expect(router.navigate).toHaveBeenCalledWith(['/app/dashboard']);
+    });
   });
 
   describe('when user provides invalid credentials', () => {
-    it.todo('should throw InvalidCredentialError');
+    
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers : [
+          LoginUserUseCase,
+          Router,
+          UserStore,
+          UserService,
+          { provide: AuthenticationService, useValue: { login: jest.fn().mockReturnValue(of(new InvalidCredentialError()))}},
+
+        ]
+      });
+      loginUserUseCase = TestBed.inject(LoginUserUseCase);
+    });
+
+    it('should throw InvalidCredentialError', async () => {
+      await expect(loginUserUseCase.execute("invalid-email", "invalid-password")).rejects.toThrow(InvalidCredentialError);
+    });
   });
 });
