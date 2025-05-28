9aed1d34b2726924ebb808631d91dbd56cb3b0f5
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-05-28 20:55:18 +0000

    feat(membership): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { HomePageComponent } from './visitor/home/home.page.component';
 import { SignupPageComponent } from './visitor/signup/signup.page.component';
 import { LoginPageComponent } from './visitor/login/login.page.component';
 import { DashboardPageComponent } from './membership/dashboard/dashboard.page.component';
+import { PlanningPageComponent } from './membership/planning/planning.page.component';
+import { ProfilePageComponent } from './membership/profile/profile.page.component';
+import { SettingsPageComponent } from './membership/settings/settings.page.component';
+import { WorkdayPageComponent } from './membership/workday/workday.page.component';
 
 export const routes: Routes = [
     {   title: 'Productivity Planner',
@@ -17,8 +21,29 @@ export const routes: Routes = [
         path: 'login',
         component: LoginPageComponent
     },
-    {   title: 'Dashboard',
+    {   
+        title: 'Dashboard',
         path: 'app/dashboard',
         component: DashboardPageComponent
-    }
+    },
+    {
+        title: 'Planning',
+        path: 'app/planning',
+        component: PlanningPageComponent,
+    },
+    {
+        title: 'Workday',
+        path: 'app/workday',
+        component: WorkdayPageComponent,
+    },
+    {
+        title: 'Profile',
+        path: 'app/profile',
+        component: ProfilePageComponent,
+    },
+    {
+        title: 'Settings',
+        path: 'app/settings',
+        component: SettingsPageComponent,
+    },
 ];
