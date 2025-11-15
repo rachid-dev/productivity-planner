2f86de0934fbbb3a1f374b0b96b89a9e1ea30078
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2025-11-15 18:46:33 +0000

    wip(workday): split readonly task

--- src/app/membership/workday/workday.page.component.ts
+++ src/app/membership/workday/workday.page.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { TaskFieldDumbComponent } from './task-field/task-field.dumb.component';
+import { TaskReadonlyDumbComponent } from './task-readonly/task-readonly.dumb.component';
 import { WorkdayStore } from './workday.page.store';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './workday.page.component.html',
-  imports: [TaskFieldDumbComponent],
+  imports: [TaskFieldDumbComponent, TaskReadonlyDumbComponent],
   providers: [WorkdayStore],
 })
 export class WorkdayPageComponent {
